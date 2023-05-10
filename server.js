aaba5f388e25101050c835756b421b7441c21f62
nisha <nisha.4r@gmail.com>
2023-05-09 19:33:39 -0600

    app listen

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const app = express();
 
 const fs = require('fs');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 
 const PORT = process.env.port || 3001;
+
+app.use(clog);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
