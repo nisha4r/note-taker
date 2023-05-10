4bf6d711b683477fe42e105ff376bd889d10ca9a
nisha <nisha.4r@gmail.com>
2023-05-09 20:31:29 -0600

    get route for anything else

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for anything else
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
